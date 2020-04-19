49e7e9ec77b59c045e415247762d573986847f15
Jagadeesh <jagadeesh@droid5.com>
2020-04-19 19:46:58 +0530

    Project 1 Project 1added building and robot model files created .world out of models wrote a plugin in c++ to display a message in console when world is opened
     Please enter the commit message for your changes. Lines starting

---
+++ Project 1/build/CMakeFiles/welcome_to_jd_house.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/welcome_to_jd_house.dir/script/welcome_to_jd_house.cpp.o"
+  "libwelcome_to_jd_house.pdb"
+  "libwelcome_to_jd_house.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/welcome_to_jd_house.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
